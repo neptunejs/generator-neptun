acb0fd6609aac9e08c262b3110fc55872b9d31c1
Daniel Kostro <kostro.d@gmail.com>
2017-01-24 16:00:16 +0100

    use node-generator

--- generators/react/index.js
+++ generators/react/index.js
@@ -4,37 +4,54 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = Generator.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the prime ' + chalk.red('generator-neptun react component') + ' generator!'
-    ));
-  },
+    initializing() {
+        this.log(yosay(
+            'Welcome to the prime ' + chalk.red('generator-neptun react component') + ' generator!'
+        ));
+    },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('src'),
-      this.destinationPath('src')
-    );
-  },
+    default() {
+        this.composeWith(require.resolve('generator-node/generators/app'), {
+            babel: false,
+            boilerplate: false,
+            projectRoot: './',
+            skipInstall: true,
+            gulp: false,
+            travis: false,
+            git: true,
+            githubAccount: 'neptunjs'
+        });
+    },
 
-  install: function () {
-    this.yarnInstall([
-      'babel-core',
-      'babel-loader',
-      'babel-preset-env',
-      'babel-preset-react',
-      'eslint',
-      'eslint-config-cheminfo',
-      'eslint-plugin-no-only-tests',
-      'eslint-plugin-react',
-      'jest-cli',
-      'npm-run-all',
-      'react',
-      'react-dom',
-      'react-test-renderer',
-      'webpack',
-      'webpack-dev-server'
-    ], {dev: true});
-  }
+
+    writing () {
+        this.fs.copy(
+            this.templatePath('.'),
+            this.destinationPath('.'),
+        );
+    },
+
+    install () {
+        this.yarnInstall([
+            'babel-core',
+            'babel-loader',
+            'babel-preset-env',
+            'babel-preset-react',
+            'eslint',
+            'eslint-config-cheminfo',
+            'eslint-plugin-no-only-tests',
+            'eslint-plugin-react',
+            'jest-cli',
+            'npm-run-all',
+            'react',
+            'react-dom',
+            'react-test-renderer',
+            'webpack',
+            'webpack-dev-server'
+        ], {dev: true});
+    },
+
+    end() {
+        console.log(this.options);
+    }
 });
